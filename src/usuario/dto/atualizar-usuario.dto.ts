a9de64cfaeaecd0db3aed32f4ea7bd7db7fa3c26
Afrânio Nunes Dantas <afraniodantas0224@gmail.com>
2026-07-26 18:18:16 -0300

    fix: add partial type in update dto

--- src/usuario/dto/atualizar-usuario.dto.ts
+++ src/usuario/dto/atualizar-usuario.dto.ts
@@ -1,21 +1,4 @@
-import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
-import { EmailEhUnico } from '../validacao/email-eh-unico.validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
+import { CriarUsuarioDTO } from './criar-usuario.dto';
 
-export class AtualizarUsuarioDTO {
-  @ApiProperty({ description: 'O nome do usuário' })
-  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
-  @IsOptional()
-  nome: string;
-
-  @ApiProperty({ description: 'O email do usuário' })
-  @IsEmail(undefined, { message: 'O e-mail informado é inválido' })
-  @EmailEhUnico({ message: 'Já existe um usuário com este e-mail' })
-  @IsOptional()
-  email: string;
-
-  @ApiProperty({ description: 'A senha do usuário' })
-  @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
-  @IsOptional()
-  senha: string;
-}
+export class AtualizarUsuarioDTO extends PartialType(CriarUsuarioDTO) {}
